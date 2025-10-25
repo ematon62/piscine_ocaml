a8b29160a3cac7f2ef16b661078c36d422fd73c0
ematon <ematon@student.42lehavre.fr>
2025-10-25 22:48:49 +0200

    Added ex03

---
+++ syntax/ex03/ft_print_alphabet.ml
@@ -0,0 +1,18 @@
+let rec ft_alphabet_iteration (c : char) : unit =
+  let _ = print_char c in
+  if (c = 'z') then
+    ()
+  else
+    let n = int_of_char c in
+    let next_char = char_of_int (n + 1) in
+    ft_alphabet_iteration next_char
+  
+
+let ft_print_alphabet () : unit =
+  let _ = ft_alphabet_iteration 'a' in
+  print_char '\n'
+
+(*
+  Main
+*)
+let _ = ft_print_alphabet ()
