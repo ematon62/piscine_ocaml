6a87e8cf802f84718c4b6d512037cc4f320b3834
ematon <ematon@student.42lehavre.fr>
2025-10-25 18:06:50 +0200

    Added more tests for ex00

--- syntax/ex00/ft_test_sign.ml
+++ syntax/ex00/ft_test_sign.ml
@@ -1,4 +1,10 @@
+let ft_test_sign (x : int) : unit = 
+  if (x < 0) then 
+    print_endline "negative" 
+  else 
+    print_endline "positive"
 
-let ft_test_sign x = match (x < 0) with
-    | true -> print_endline "negative"
-    | false -> print_endline "positive"
+let test (x : int) : unit = 
+  print_string ("Sign of " ^ (string_of_int x) ^ "? -> "); ft_test_sign x
+
+let () = test 0; test 1; test (-1)
