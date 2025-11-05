027fb983843e6b5ec9eb3f8a9696d0da2c96f526
ematon <ematon@student.42lehavre.fr>
2025-11-05 21:46:10 +0100

    ex02 done

---
+++ recursion/ex02/ackermann.ml
@@ -0,0 +1,18 @@
+let rec ackermann (m : int) (n : int) : int =
+  if (m < 0 || n < 0) then
+    (-1)
+  else if (m = 0) then
+    (n + 1)
+  else if (m > 0 && n = 0) then
+    ackermann (m - 1) 1
+  else
+    ackermann (m - 1) (ackermann m (n - 1))
+
+(*
+    Main
+*)
+let test x y =
+  let _ = print_string ("ackermann " ^ (string_of_int x) ^ " " ^ (string_of_int y) ^ ": ") in
+  print_endline (string_of_int (ackermann x y))
+
+let _ = test (-1) 7; test 0 0; test 2 3; test 4 1
