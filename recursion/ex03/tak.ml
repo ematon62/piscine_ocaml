aca87e126dfe3acac4a1d9cdb8b4920497ac7127
ematon <ematon@student.42lehavre.fr>
2025-11-05 21:52:36 +0100

    ex03 done

---
+++ recursion/ex03/tak.ml
@@ -0,0 +1,14 @@
+(*
+  tak(tak(x − 1, y, z), tak(y − 1, z, x), tak(z − 1, x, y))
+*)
+let rec tak (x : int) (y : int) (z : int): int =
+  if (y < x) then
+    tak (tak (x - 1) y z) (tak (y - 1) z x) (tak (z - 1) x y)
+  else
+    z
+  
+let test x y z =
+  let _ = print_string ("tak " ^ (string_of_int x) ^ " " ^ (string_of_int y) ^ " " ^ (string_of_int z) ^ ": ") in
+  print_endline (string_of_int (tak x y z))
+
+let _ = test 1 2 3; test 5 23 7; test 9 1 0; test 1 1 1; test 0 42 0; test 23498 98734 98776;
