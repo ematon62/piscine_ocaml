3be17b05b94e62fce7cc1eaa0a65f5a52159cb2d
Your Name <you@example.com>
2025-11-06 22:54:35 +0100

    ex08 done, ex09 ?

---
+++ recursion/ex08/ft_sum.ml
@@ -0,0 +1,24 @@
+let ft_sum (f: int -> float) (lower: int) (upper: int) : float =
+  let rec ft_sum_aux (n : float) (m : int) = 
+    if (m = upper) then
+      (f m) +. n
+    else
+      ft_sum_aux ((f m) +. n) (m + 1) in
+  if (upper < lower) then nan else ft_sum_aux 0. lower
+
+(*
+  Main
+*)
+let square x = float_of_int (x * x)
+let identity x = float_of_int x
+let reciprocal x = 1. /. float_of_int x
+
+(* Test cases *)
+let () =
+  Printf.printf "Sum of squares from 1 to 5 = %f\n" (ft_sum square 1 5);
+  Printf.printf "Sum of identity from 3 to 7 = %f\n" (ft_sum identity 3 7);
+  Printf.printf "Sum of reciprocals from 1 to 3 = %f\n" (ft_sum reciprocal 1 3);
+  Printf.printf "Upper < Lower (5 to 2) = %f\n" (ft_sum square 5 2);
+
+  Printf.printf "Sum of identity from 1 to 1_000_000 = %f\n"
+    (ft_sum identity 1 1_000_000)
---
+++ recursion/ex09/leibniz_pi.ml
@@ -0,0 +1,15 @@
+let pi = float_of_int 4 *. atan 1.
+
+let f (i : int) : float = (float_of_int (-1) ** float_of_int i) /. float_of_int (2 * i + 1)
+
+let abs_fl (x : float) : float = if x < 0. then -1. *. x else x
+
+let leibniz_pi (delta : float) : int = 
+  let rec leibniz_aux (sum : float) (n : int) = 
+    let new_sum = sum +. f n in
+    let gap = abs_fl (4. *. new_sum -. pi) in
+    if gap <= delta then
+      n
+    else
+      leibniz_aux new_sum (n + 1) in
+  if delta < 0. then (-1) else leibniz_aux 0. 0
