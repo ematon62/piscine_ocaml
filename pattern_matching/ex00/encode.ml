3c3eb4ca5ae769e578d27db5a2b4e88f9131b8bc
ematon <emanuel.maton1@gmail.com>
2025-11-13 17:01:23 +0100

    pattern: replace @ with :: operator

--- pattern_matching/ex00/encode.ml
+++ pattern_matching/ex00/encode.ml
@@ -1,29 +1,31 @@
 let encode (l: 'a list) : (int * 'a) list = 
-  let rec encode_aux (l : 'a list) (t : (int * 'a)) (out : (int * 'a) list) =
+  let rec encode_aux (l : 'a list) (t : (int * 'a)) =
     match (l, t) with
-    | [],_ -> out @ [t]
-    | hd::tl, t when (snd t = hd) -> encode_aux tl (fst t + 1, snd t) out
-    | hd::tl,_ -> let with_tuple = out @ [t] in encode_aux tl (1, hd) with_tuple
+    | [],_ -> [t]
+    | hd::tl, t when (snd t = hd) -> encode_aux tl (fst t + 1, snd t)
+    | hd::tl,_ -> t::(encode_aux tl (1, hd))
   in match l with
   | [] -> []
-  | hd::tl -> encode_aux l (0, hd) []
+  | hd::tl -> encode_aux l (0, hd)
 
-let test_char l = 
-	let rec test_aux l = match l with
-	| [] -> print_newline ()
-	| hd::tl -> Printf.printf "%d%c " (fst hd) (snd hd); test_aux tl in
-	test_aux (encode l)
-
-let test_int l = 
-	let rec test_aux l = match l with
-	| [] -> print_newline ()
-	| hd::tl -> Printf.printf "%d%d " (fst hd) (snd hd); test_aux tl in
-	test_aux (encode l)
-
+	
 let () =
+	let test_char l = 
+		let rec test_aux l = match l with
+		| [] -> print_newline ()
+		| hd::tl -> Printf.printf "%d%c " (fst hd) (snd hd); test_aux tl
+	in Printf.printf "test char: "; test_aux (encode l) in
+	
+	let test_int l = 
+		let rec test_aux l = match l with
+		| [] -> print_newline ()
+		| hd::tl -> Printf.printf "%d%d " (fst hd) (snd hd); test_aux tl
+	in Printf.printf "test int: "; test_aux (encode l) in
+
 	test_char ['a';'a';'a';'b';'b';'b';];
 	test_char [];
 	test_char ['1';'2';'3';'4'];
 	test_char ['1';'1';'1';'1'];
 	test_int [1;2;3;4];
 	test_int [1;1;1;2;2;2;3;3;3;4;4;4;4;4;4;4;4];
+	test_int []
--- pattern_matching/ex01/crossover.ml
+++ pattern_matching/ex01/crossover.ml
@@ -2,28 +2,44 @@ let crossover (l1 : 'a list) (l2 : 'a list) : 'a list =
   let rec is_in_list (elem : 'a) (l : 'a list) : bool = match l with
     | [] -> false
     | hd::tl -> if hd = elem then true else is_in_list elem tl in
-  let rec crossover_aux l_1 l_2 commons = match l_1 with
-    | [] -> commons
-    | hd::tl -> crossover_aux tl l_2 (if ((is_in_list hd l_2) && not (is_in_list hd commons)) then 
-      commons @ [hd] 
-    else commons) in crossover_aux l1 l2 []
+
+  let rec crossover_aux l_1 l_2 = match l_1 with
+    | [] -> []
+    | hd::tl -> if ((is_in_list hd l_2)) then
+      hd::(crossover_aux tl l2) else crossover_aux tl l2
+  in crossover_aux l1 l2
 
 (*
   Main
 *)
-let test_char l1 l2 = List.iter (Printf.printf "%c") (crossover l1 l2); print_newline ()
-let test_int l1 l2= List.iter (Printf.printf "%d") (crossover l1 l2); print_newline ()
+let () = 
+  let test_char l1 l2 =
+  Printf.printf "test char:\nl1 = ";
+  List.iter (Printf.printf "%c ") l1;
+  Printf.printf "\nl2 = [";
+  List.iter (Printf.printf "%c ") l2;
+  Printf.printf "\nresult = ";
+  List.iter (Printf.printf "%c ") (crossover l1 l2); 
+  Printf.printf "\n\n"; in
 
-let _ = 
+  let test_int l1 l2 =
+  Printf.printf "test int:\nl1 = ";
+  List.iter (Printf.printf "%d ") l1;
+  Printf.printf "\nl2 = ";
+  List.iter (Printf.printf "%d ") l2;
+  Printf.printf "\nresult = ";
+  List.iter (Printf.printf "%d ") (crossover l1 l2); 
+  Printf.printf "\n\n"; in
+
   test_char [] [];
+  test_char ['a';'a';'b';] [];
+  test_char [] ['a';'a';'b';];
   test_char ['a'] ['b';'c'];
   test_char ['b';'b';] ['b';'c'];
   test_char ['a';'b';'c';] ['a';'b';'c';];
   test_char ['a';'a';'b';] ['b';'a';'a';];
-  test_char ['a';'a';'b';] [];
-  test_char [] ['a';'a';'b';];
   test_int [] [];
   test_int [1] [2;3];
   test_int [2;] [3;2];
-  test_int [1;2;3] [1];
+  test_int [1;2;2;2;2;2;2;1;3] [1;2];
   test_int [1;2;3] [1;2;3];
