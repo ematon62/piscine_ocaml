7fc153fa575abd06397aa3ba9cf0251f9bc255cd
Your Name <you@example.com>
2025-11-24 00:13:42 +0100

    conway done

--- pattern_matching/ex03/sequence.ml
+++ pattern_matching/ex03/sequence.ml
@@ -1,11 +1,32 @@
-let encode (l: 'a list) : (int * 'a) list = 
-  let rec encode_aux (l : 'a list) (t : (int * 'a)) =
-    match (l, t) with
-    | [],_ -> [t]
-    | hd::tl, t when (snd t = hd) -> encode_aux tl (fst t + 1, snd t)
-    | hd::tl,_ -> t::(encode_aux tl (1, hd))
-  in match l with
-  | [] -> []
-  | hd::tl -> encode_aux l (0, hd)
+let rec int_list_to_string l = match l with
+  | [] -> ""
+  | hd::tl -> (string_of_int hd) ^ (int_list_to_string tl)
 
+let sequence (n : int) : string = 
+  let rec next (l : int list) = match l with
+    | [] -> []
+    | h1::h2::h3::tl when h1 = h2 && h2 = h3 -> 3::h1::(next tl)
+    | h1::h2::tl when h1 = h2 -> 2::h1::(next tl)
+    | hd::tl -> 1::hd::(next tl) in
   
+  let rec aux (k : int) (l : int list) : int list = match k with
+    | _ when k = n -> l
+    | _ -> aux (k +1) (next l) in
+
+  if (n <= 0) then "" else int_list_to_string (aux 1 [1])
+
+(*
+  Main
+*)
+
+let () = 
+  let test n = Printf.printf "%d-th term of Conway sequence: %s\n" n (sequence n) in
+  test (-1);
+  test 0;
+  test 1;
+  test 2;
+  test 3;
+  test 4;
+  test 5;
+  test 6;
+  test 7;
